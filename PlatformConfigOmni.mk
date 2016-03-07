f628aa0fce80977a78990e5ad1b28ac8a7a02515
Christian Oder <myself5@carbonrom.org>
2016-03-07 16:50:19 +0100

    kitakami: add multirom support
    Change-Id: I1069ae3d561f26cae7f2bb50c4a25ff2d4eae0c2

---
+++ PlatformConfigOmni.mk
@@ -0,0 +1,26 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include device/sony/kitakami-common/PlatformConfig.mk
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/sony/kitakami-common/multirom/init_devices.c
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := device/sony/kitakami-common/multirom/framebuffer_qcom_overlay.h
+MR_KEXEC_DTB := true
+MR_FSTAB := device/sony/kitakami-common/rootdir/twrp.fstab
+MR_USE_MROM_FSTAB := true
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+MR_PIXEL_FORMAT := "RGBX_8888"
