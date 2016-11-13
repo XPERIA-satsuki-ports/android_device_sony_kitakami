2551ebf8006c179637f7a314cb51bc17520a9908
Shun Terabayashi <shunonymous@gmail.com>
2016-11-13 18:25:17 +0900

    Adjust for MultiROM

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -76,6 +76,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 ### TWRP ###
 ifeq ($(RECOVERY_VARIANT),twrp)
+TARGET_RECOVERY_IS_MULTIROM := true
 TARGET_TWRP_FSTAB := true
 PROJECT_PATH_AGREES := true
 RECOVERY_SDCARD_ON_DATA := true
@@ -95,6 +96,17 @@ endif
 ifneq ($(TARGET_TWRP_FSTAB),true)
 TARGET_RECOVERY_FSTAB = device/sony/kitakami/rootdir/fstab.kitakami
 endif
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/sony/kitakami/multirom/init_devices.c
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := device/sony/kitakami/multirom/framebuffer_qcom_overlay.h
+MR_KEXEC_DTB := true
+MR_FSTAB := device/sony/kitakami/rootdir/twrp.fstab
+MR_USE_MROM_FSTAB := true
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+MR_PIXEL_FORMAT := "RGBX_8888"
 
 # Wi-Fi definitions for Broadcom solution
 BOARD_WLAN_DEVICE           := bcmdhd
